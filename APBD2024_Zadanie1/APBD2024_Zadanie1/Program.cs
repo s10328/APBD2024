29b3583cd20ddb7cdc3dae8519a4b46051267991
s10328 <s10328@pjwstk.edu.pl>
2024-03-24 19:36:42 +0100

    Commit branchu feature-average

--- APBD2024_Zadanie1/APBD2024_Zadanie1/Program.cs
+++ APBD2024_Zadanie1/APBD2024_Zadanie1/Program.cs
@@ -5,3 +5,20 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Modyfikacja 1");
 Console.WriteLine("Modyfikacja 2");
 Console.WriteLine("Modyfikacja 2");
+
+double CalculateAverage(int[] numbers)
+{
+    if (numbers == null || numbers.Length == 0)
+    {
+        throw new ArgumentException("Tablica nie może być pusta.");
+    }
+
+    long sum = 0;
+    foreach (int number in numbers)
+    {
+        sum += number;
+    }
+
+    return (double)sum / numbers.Length;
+}
+
